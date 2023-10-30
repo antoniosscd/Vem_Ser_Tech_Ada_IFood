ea275421703db5092c493782c1f75920d8087a1a
Antonio Sousa <antoniosscd@gmail.com>
2023-10-29 23:10:58 -0300

    delete

--- Modulo_Logica_de_Programacao_I/converte_temperatura.py
+++
@@ -1,9 +0,0 @@
-# 9. Faça um programa que peça a temperatura em graus Fahrenheit (F), transforme e mostre a temperatura em graus Celsius (°C).
-# C = (5 * (F-32) / 9).
-
-#Solicita a temperatura em Fahrenheit (F).
-temp = float(input("Informe a temperatura na escala Fahrenheit: "))
-
-temp_convert = (5 * (temp-32) / 9) # Conversão em Celsius(°C).
-
-print(f'Temperatura informada em graus Celsius (°C) é igual a {temp_convert}°C ')#Imprime a temperatura na escala Celcius.
